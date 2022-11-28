36b1c38cda7c5b15da2db61c41cce70a549884ed
AnReshetilova <antonina.reshetilova@gmail.com>
2022-11-28 23:43:55 +0300

    lab9

--- lab9/WebDriverTests/WebDriverTests/UnitTest1.cs
+++ lab9/WebDriverTests/WebDriverTests/UnitTest1.cs
@@ -10,7 +10,12 @@ namespace WebDriverTests
     public class Tests
     {
         private IWebDriver driver;
-        private readonly By CatalogButton = By.XPath("//a[text()=' ‡Ú‡ÎÓ„']");
+        private readonly By logoButton = By.XPath("//img[@class='header-logo-image logo']");
+        private readonly By dropdownCatalogButton = By.XPath("//button[@class='btn btn-primary dropdown-toggle']");
+        private readonly By shirtsCatalogButton = By.XPath("//a[text()='–”¡¿ÿ »']");
+        private readonly By shirtsComfortCatalogButton = By.XPath("/html/body/div[2]/div/div[1]/ul/li[3]/a");
+        private readonly By accessoriesCatalogButton = By.XPath("//a[text()='¿ —≈——”¿–€']");
+        private readonly By hatsAccessoriesCatalogButton = By.XPath("/html/body/div[2]/div/div[1]/ul/li[2]/a");
 
         [SetUp]
         public void Setup()
@@ -20,19 +25,32 @@ namespace WebDriverTests
         }
 
         [Test]
-        public void CatalogButtonTest()
+        public void LogoButtonTest()
         {
-            var catalog = driver.FindElement(CatalogButton);
+            var catalog = driver.FindElement(logoButton);
             catalog.Click();
         }
 
         [Test]
-        public void WomenButtonTest()
+        public void ComfortShirtsButtonTest()
         {
-            var catalog = driver.FindElement(CatalogButton);
+            var catalog = driver.FindElement(dropdownCatalogButton);
             catalog.Click();
-            var women = driver.FindElement(By.LinkText("∆≈ÕŸ»Õ¿Ã"));
-            women.Click();
+            var shirts = driver.FindElement(shirtsCatalogButton);
+            shirts.Click();
+            var shirtsComfort = driver.FindElement(shirtsComfortCatalogButton);
+            shirtsComfort.Click();
+        }
+
+        [Test]
+        public void CheapFirstComfortShirtsButtonTest()
+        {
+            var catalog = driver.FindElement(dropdownCatalogButton);
+            catalog.Click();
+            var accessories = driver.FindElement(accessoriesCatalogButton);
+            accessories.Click();
+            var hats = driver.FindElement(hatsAccessoriesCatalogButton);
+            hats.Click();
         }
 
         [TearDown]
